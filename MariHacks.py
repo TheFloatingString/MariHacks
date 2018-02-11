5c7f82467196010862f2bad706ef524605786135
TheFloatingString <laurencel2001@gmail.com>
2018-02-11 14:49:41 -0500

    Update MariHacks.py

--- MariHacks.py
+++ MariHacks.py
@@ -9,9 +9,9 @@ import urllib.request
 import os
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-sentence_input = input()
+sentence_input = input()            # input sentence
 sentence_string = sentence_input.lower()
-words = sentence_string.strip('.",!?-=').split()
+words = sentence_string.strip('.",!?-=').split()        # split sentence into lists
 
 ##response = urllib.request.urlopen('http://wordnetweb.princeton.edu/perl/webwn?s=running&sub=Search+WordNet&o2=&o0=1&o8=1&o1=&o7=&o5=&o9=&o6=&o3=&o4=&h=0000000000000000000000000000000000000000000000000000')
 ##html = response.read()
@@ -25,7 +25,7 @@ useless_words = str(useless_words_file.read())
 ##translation = VideoFileClip(word[0] + '.mp4')
 ##translation_file = VideoFileClip('translation.mp4')
 cwd = os.getcwd()
-clips = []
+clips = []          # list for clips
 
 for word in words:
     if word in useless_words:
@@ -42,8 +42,8 @@ for word in words:
         useless_words += ' ' + word
         continue
 
-os.chdir(cwd)
-useless_words_file.close()
+os.chdir(cwd) 
+useless_words_file.close()          # close file
 concatenated_clips = concatenate_videoclips(clips)
 ##os.remove('translation.mp4')
 concatenated_clips.write_videofile('translation.mp4')
